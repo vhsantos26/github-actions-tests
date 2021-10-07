4b1337bc049a9eff2d29d06b74404429b874e123
vhsantos26 <vhsantos26@gmail.com>
2021-10-07 17:38:36 -0300

    feat: add codepush-validation action

--- .github/github-actions/codepush-validation/entrypoint.sh
+++ .github/github-actions/codepush-validation/entrypoint.sh
@@ -6,25 +6,25 @@ while read git_diff_line; do
   echo "::debug::LINE: $git_diff_line "
 
   if [[ "$git_diff_line" == "$INPUT_PACKAGE_JSON_PATH"* ]]; then
-    echo "::warning::Skip CodePush deployment as branch may not contains only react-native changes (file: $get_diff_line)."
+    echo "::warning title=$git_diff_line::Skip CodePush deployment as branch may not contains only react-native changes."
     echo "::set-output name=codepush_deployment::false"
     exit 0
   fi
 
   if [[ "$git_diff_line" == "$INPUT_ANDROID_PATH"* ]]; then
-    echo "::warning::Skip CodePush deployment as branch may not contains only react-native changes (file: $get_diff_line)."
+    echo "::warning title=$git_diff_line::Skip CodePush deployment as branch may not contains only react-native changes."
     echo "::set-output name=codepush_deployment::false"
     exit 0
   fi
 
   if [[ "$git_diff_line" == "$INPUT_IOS_PATH"* ]]; then
-    echo "::warning::Skip CodePush deployment as branch may not contains only react-native changes (file: $get_diff_line)."
+    echo "::warning title=$git_diff_line::Skip CodePush deployment as branch may not contains only react-native changes."
     echo "::set-output name=codepush_deployment::false"
     exit 0
   fi
 
   if [[ "$git_diff_line" == "$INPUT_YARN_LOCK_PATH"* ]]; then
-    echo "::warning::Skip CodePush deployment as branch may not contains only react-native changes (file: $get_diff_line)."
+    echo "::warning title=$git_diff_line::Skip CodePush deployment as branch may not contains only react-native changes."
     echo "::set-output name=codepush_deployment::false"
     exit 0
   fi
